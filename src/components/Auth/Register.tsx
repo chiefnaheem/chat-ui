aff5e95fb2a1edbf52d7fda0d15779e0d68a22b9
chiefnaheem <naheemademola21@gmail.com>
2023-01-16 10:57:49 -0800

    updates

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,34 +1,42 @@
-import React from 'react'
-import { Button, InputContainer, InputField, InputLabel } from '../../styles'
-import styles from './index.module.scss'
-
+import React from "react";
+import { Button, InputContainer, InputField, InputLabel } from "../../styles";
+import styles from "./index.module.scss";
+import { Link } from "react-router-dom";
 
 export default function Register() {
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log('submit')
-    }
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit");
+  };
   return (
     <form className={styles.form}>
+      <InputContainer>
+        <InputLabel htmlFor="email">Email</InputLabel>
+        <InputField type="email" id="email" />
+      </InputContainer>
+      <section className={styles.nameFieldRow}>
         <InputContainer>
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <InputField type="email" id='email' />
+          <InputLabel htmlFor="firstName">First Name</InputLabel>
+          <InputField type="text" id="firstName" />
         </InputContainer>
-        <section className={styles.nameFieldRow}>
-        <InputContainer >
-            <InputLabel htmlFor="firstName">First Name</InputLabel>
-            <InputField type="text" id='firstName' />
+        <InputContainer>
+          <InputLabel htmlFor="lastName">Last Name</InputLabel>
+          <InputField type="text" id="lastName" />
         </InputContainer>
-        <InputContainer >
-            <InputLabel htmlFor="lastName">Last Name</InputLabel>
-            <InputField type="text" id='lastName' />
-        </InputContainer>
-        </section>
-        <InputContainer >
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <InputField type="password" id='password' />
-        </InputContainer>
-        <Button type="submit" className={styles.button}>Register</Button>
+      </section>
+      <InputContainer>
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <InputField type="password" id="password" />
+      </InputContainer>
+      <Button type="submit" className={styles.button}>
+        Register
+      </Button>
+      <div className={styles.userExist}>
+        <p>
+          Already have an account?
+          <Link to="/login">Login</Link>
+        </p>
+      </div>
     </form>
-  )
+  );
 }
