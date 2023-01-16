444d59e0bed965a00436a779097c46c8d7e01570
chiefnaheem <naheemademola21@gmail.com>
2023-01-16 10:29:43 -0800

    login page updates

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Button, InputContainer, InputField, InputLabel } from '../../styles'
+import styles from './index.module.scss'
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <form className={styles.form}>
+            <InputContainer>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <InputField type="email" id='email' />
+            </InputContainer>
+
+            <InputContainer className={styles.loginFormPassword}>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <InputField type="password" id='password' />
+            </InputContainer>
+            <Button type="submit" className={styles.button}>Login</Button>
+        </form>
+      )
 }
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Register from '../../components/Auth/Register'
+import Login from '../../components/Auth/Login'
 import { Page } from '../../styles'
 
 export default function LoginPage() {
   return (
     <Page>
-        <Register />
+        <Login />
     </Page>
   )
 }
